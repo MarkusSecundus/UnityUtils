d4e9158fedd8287fd115b1b23cc49262d1f2430f
MarkusSecundus <kubahronik.g@gmail.com>
2024-04-30 11:55:00 +0200

    add Input section

---
+++ Input/BasicInputAxisSupplier.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace MarkusSecundus.Utils.Input
+{
+    public class BasicInputAxisSupplier : InputAxisSupplier
+    {
+        [SerializeField] string Axis;
+        [SerializeField] bool UseRaw;
+        public override float Value => UseRaw ? UnityEngine.Input.GetAxisRaw(Axis) : UnityEngine.Input.GetAxis(Axis);
+    }
+}
---
+++ Input/InputAxisSupplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MarkusSecundus.Utils.Input
+{
+    public interface IInputAxisSupplier
+    {
+        public float Value { get; }
+    }
+    public abstract class InputAxisSupplier : MonoBehaviour, IInputAxisSupplier
+    {
+        public abstract float Value { get; }
+    }
+}

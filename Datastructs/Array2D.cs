9ee06593a6d58c2be190d37a4ce81f50d4eb25d6
MarkusSecundus <kubahronik.g@gmail.com>
2024-05-04 17:08:38 +0200

    add Array2D<T> - 2D array view for a simple 1D array

---
+++ Datastructs/Array2D.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MarkusSecundus.Utils.Assets._Scripts.Utils.Datastructs
+{
+    [System.Serializable]public struct Array2D<T>
+    {
+        [field: SerializeField]public T[] BackingArray { get; }
+        [field: SerializeField] public int Width { get; }
+        public int Height => BackingArray.Length / Width;
+
+        public ref T this[int x, int y] => ref BackingArray[y*Width + x];
+
+        public Array2D(T[] baseArray, int width)
+        {
+            if (baseArray.Length % width != 0)
+                throw new System.ArgumentException($"Cannot create 2D array - length {baseArray.Length} of base array is not divisible by width {width}");
+            (BackingArray, Width) = (baseArray, width);
+        }
+        public Array2D(int width, int height) : this(new T[width*height], width) { }
+
+
+        public IEnumerable<IEnumerable<T>> IterateLines()
+        {
+            var self = this;
+            for (int y = 0; y < self.Height; ++y) yield return line(y);
+
+            IEnumerable<T> line(int y)
+            {
+                for (int x = 0; x < self.Width; ++x)
+                    yield return self[x, y];
+            }
+        }
+    }
+}

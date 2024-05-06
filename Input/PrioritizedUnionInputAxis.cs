778ae99f8e02e8dcc7f05ed224c486b1bdc8db04
MarkusSecundus <kubahronik.g@gmail.com>
2024-05-06 10:14:33 +0200

    PrioritizedUnionInputAxis - stricter device determining policy

--- Input/PrioritizedUnionInputAxis.cs
+++ Input/PrioritizedUnionInputAxis.cs
@@ -8,6 +8,25 @@ namespace MarkusSecundus.Utils.Input
     {
         [SerializeField] InputAxisSupplier[] _axes;
 
-        public override float Value => _axes.FirstOrDefault(a => a.Value != 0f)?.Value ?? 0f;
+        int _activeAxisIndex = -1;
+        private void Awake()
+        {
+            _activeAxisIndex = _axes.Length - 1;
+        }
+        public override float Value { get
+            {
+                if (_activeAxisIndex < 0) return 0f;
+                for (int t = 0; t < _activeAxisIndex; ++t)
+                {
+                    var axis = _axes[t].Value;
+                    if(axis != 0)
+                    {
+                        _activeAxisIndex = t;
+                        return axis;
+                    }
+                }
+                return _axes[_activeAxisIndex].Value;
+            } }
+
     }
 }

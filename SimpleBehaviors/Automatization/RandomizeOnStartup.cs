fcee6fd14d11ce0bef0c765ca9ad86c1f092724f
MarkusSecundus <kubahronik.g@gmail.com>
2025-03-13 10:41:01 +0100

    add RandomizeOnStartup

---
+++ SimpleBehaviors/Automatization/RandomizeOnStartup.cs
@@ -0,0 +1,22 @@
+﻿using MarkusSecundus.Utils.Behaviors.Automatization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MarkusSecundus.Utils.Assets.Scripts.Utils.SimpleBehaviors.Automatization
+{
+    public class RandomizeOnStartup : MonoBehaviour
+    {
+        [SerializeField] int Seed = -1;
+        [SerializeField] bool IncludeInactive = true;
+
+        private void Start()
+        {
+            System.Random rand = Seed == -1 ? new System.Random() : new System.Random(Seed);
+            IRandomizer.RandomizeAll(gameObject, rand, IncludeInactive);
+        }
+    }
+}
